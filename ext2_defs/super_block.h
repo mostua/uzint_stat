d74bc5a642bf46dc373646631ab0821cf175b6f8
mostua <mostua@gmail.com>
2015-05-17 04:23:43 +0200

    Prosta funkcjonalnosc

--- ext2_defs/super_block.h
+++ ext2_defs/super_block.h
@@ -4,31 +4,31 @@
 
 struct super_block
 {
-	int s_inodes_count;
-	int s_blocks_count;
-	int s_r_blocks_count;
-	int s_free_blocks_count;
-	int s_free_inodes_count;
-	int s_first_data_block;
-	int s_log_block_size;
-	int s_log_frag_size;
-	int s_blocks_per_group;
-	int s_frags_per_group;
-	int s_inodes_per_group;
-	int s_mtime;
-	int s_wtime;
-	short s_mnt_count;
-	short s_max_mnt_count;
-	short s_magic;
-	short s_state;
-	short s_errors;
-	short s_minor_rev_level;
-	int s_lastcheck;
-	int s_checkinerval;
-	int s_creator_os;
-	int s_rev_level;
-	short s_def_resuid;
-	short s_def_resgid;
+	u32 s_inodes_count;
+	u32 s_blocks_count;
+	u32 s_r_blocks_count;
+	u32 s_free_blocks_count;
+	u32 s_free_inodes_count;
+	u32 s_first_data_block;
+	u32 s_log_block_size;
+	u32 s_log_frag_size;
+	u32 s_blocks_per_group;
+	u32 s_frags_per_group;
+	u32 s_inodes_per_group;
+	u32 s_mtime;
+	u32 s_wtime;
+	u16 s_mnt_count;
+	u16 s_max_mnt_count;
+	u16 s_magic;
+	u16 s_state;
+	u16 s_errors;
+	u16 s_minor_rev_level;
+	u32 s_lastcheck;
+	u32 s_checkinerval;
+	u32 s_creator_os;
+	u32 s_rev_level;
+	u16 s_def_resuid;
+	u16 s_def_resgid;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 #include "ext2_defs/inode.h"
 #include "ext2_defs/block_group.h"
 #include "ext2_defs/block_group.h"
+#include <cstring>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+const int BOOT_BLOCK_SIZE = 1024;
+
+char buffer[1024];
 
 int main()
 {
 	super_block sb;
 	inode in;
 	block_group bg;
+
+    ifstream partition("ext2_part", ifstream::binary);
+    partition.seekg(BOOT_BLOCK_SIZE);
+    partition.read(buffer, sizeof(sb));
+    memcpy (&sb, buffer, sizeof(sb));
+    cout << sb.s_blocks_count << endl;
+    cout << sb.s_free_blocks_count << endl;
+    cout << sb.s_r_blocks_count << endl;
 }
